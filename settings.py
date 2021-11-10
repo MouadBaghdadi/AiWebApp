52ddb7a28c65cc9360c2991341131160e62fb8ae
suumokr <notedev.jp@gmail.com>
2021-11-10 11:21:18 +0900

    edit google

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     APP_ENV: str = 'dev'
     GOOGLE_CLIENT_ID: str
     GOOGLE_SECRET: str
+    GOOGLE_URL: str = None
     SECRET: str
     authjwt_secret_key: str
     authjwt_token_location: set = {'cookies'}
